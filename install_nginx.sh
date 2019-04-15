7c3e0c924cb79f440bd781641deafe82bcd2de0f
linus.zhou <342174066@qq.com>
2019-04-15 11:35:22 +0800

    remove \n

--- install_nginx.sh
+++ install_nginx.sh
@@ -3,10 +3,10 @@
 # 安装nginx
 touch /etc/yum.repos.d/nginx.repo
 
-echo '[nginx]\n' > /etc/yum.repos.d/nginx.repo
-echo 'name=nginx repo \n' >> /etc/yum.repos.d/nginx.repo
-echo 'baseurl=http://nginx.org/packages/centos/7/\$basearch/' >> /etc/yum.repos.d/nginx.repo
-echo 'gpgcheck=0 \n' >> /etc/yum.repos.d/nginx.repo
+echo '[nginx]' > /etc/yum.repos.d/nginx.repo
+echo 'name=nginx repo' >> /etc/yum.repos.d/nginx.repo
+echo 'baseurl=http://nginx.org/packages/centos/7/$basearch/' >> /etc/yum.repos.d/nginx.repo
+echo 'gpgcheck=0' >> /etc/yum.repos.d/nginx.repo
 echo 'enabled=1' >> /etc/yum.repos.d/nginx.repo
 
 yum update -y
